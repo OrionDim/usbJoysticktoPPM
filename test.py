47c9cbfd10e5b97e691f28d364c83f54988aa1fe
Dmitry Kozlov <lanarion@gmail.com>
2022-01-18 08:40:54 +0300

    test PPM output

--- test.py
+++ test.py
@@ -1,33 +1,32 @@
+#!/usr/bin/env python
 
-if __name__ == "__main__":
+import time
+import PPM
+import pigpio
 
-    import time
-    import PPM
-    import pigpio
+pi = pigpio.pi()
 
-    pi = pigpio.pi()
+if not pi.connected:
+    exit(0)
 
-    if not pi.connected:
-        exit(0)
+pi.wave_tx_stop()  # Start with a clean slate.
 
-    pi.wave_tx_stop()  # Start with a clean slate.
+ppm = PPM.X(pi, 4, frame_ms=20)
 
-    ppm = PPM.X(pi, 4, frame_ms=20)
+updates = 0
+start = time.time()
+for chan in range(8):
+    for pw in range(1000, 2000, 5):
+        ppm.update_channel(chan, pw)
+        updates += 1
+end = time.time()
+secs = end - start
+print("{} updates in {:.1f} seconds ({}/s)".format(updates, secs, int(updates / secs)))
 
-    updates = 0
-    start = time.time()
-    for chan in range(8):
-        for pw in range(1000, 2000, 5):
-            ppm.update_channel(chan, pw)
-            updates += 1
-    end = time.time()
-    secs = end - start
-    print("{} updates in {:.1f} seconds ({}/s)".format(updates, secs, int(updates / secs)))
+ppm.update_channels([1000, 2000, 1000, 2000, 1000, 2000, 1000, 2000])
 
-    ppm.update_channels([1000, 2000, 1000, 2000, 1000, 2000, 1000, 2000])
+time.sleep(2)
 
-    time.sleep(2)
+ppm.cancel()
 
-    ppm.cancel()
-
-    pi.stop()
+pi.stop()
